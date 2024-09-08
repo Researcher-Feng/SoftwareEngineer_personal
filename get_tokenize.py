c887397cf63b469a90fc3201f2c1687d2f6d9c8f
mapleLeavesFeng <1994615860@qq.com>
2024-09-08 20:41:41 +0800

    2nd commit: improve functions: tokenization

--- get_tokenize.py
+++ get_tokenize.py
@@ -3,26 +3,34 @@ UNK = -1
 
 
 def txt_loader(file_raw, file_new):
-    with open(file_raw, 'r') as f1:
+    full_text1 = read_full_txt(file_raw)
+    full_text2 = read_full_txt(file_new)
+    return full_text1, full_text2
+
+
+def read_full_txt(file_path):
+    with open(file_path, 'r', encoding='utf-8') as f1:
         txt1 = f1.readlines()
-    with open(file_new, 'r') as f2:
-        txt2 = f2.readlines()
-    return txt1, txt2
+    full_text = ''
+    for i in txt1:
+        full_text = full_text + i
+    return full_text
 
 
 def split_txt(text):
-    seg_list = jieba.cut(text, cut_all=False)
+    seg_list = jieba.cut(text, cut_all=True)
     return seg_list
 
 
 def get_vocab(token_list, vo):
-    vo.update(list(token_list))
-    return vo
+    t_list = list(token_list)
+    vo.update(t_list)
+    return vo, t_list
 
 
 def generate_vocab(vocab_current):
     vocab_index = {'UNK':-1}
-    for i, item in enumerate(vocab_current):
+    for i, item in enumerate(vocab_current.most_common()):
         vocab_index[item[0]] = i
     return vocab_index
 
@@ -32,6 +40,10 @@ def index_token(token_list, vocab_index):
     for i, item in enumerate(token_list):
         new_token_list.append(vocab_index.get(item, UNK))
     return new_token_list
+
+
+def generate_string(token_list):
+    return ' '.join([str(i) for i in token_list])
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-from get_tokenize import txt_loader, split_txt, get_vocab, generate_vocab, index_token
+from get_tokenize import txt_loader, split_txt, get_vocab, generate_vocab, index_token, generate_string
 from google_bleu import corpus_bleu
 from rouge import Rouge
-
+import Levenshtein
 from collections import Counter
+import argparse
 
 
 def eval_accuracies(hypotheses, references):
@@ -25,26 +26,41 @@ def eval_accuracies(hypotheses, references):
     return bleu * 100, rouge_l * 100
 
 
+def levenshtein_similarity(str1, str2):
+    distance = Levenshtein.distance(str1, str2)
+    similarity = 1 - (distance / max(len(str1), len(str2)))
+    return similarity
+
+
 def main_process(file_raw, file_new):
     tx_raw, tx_new = txt_loader(file_raw, file_new)
     token_raw = split_txt(tx_raw)
     token_new = split_txt(tx_new)
     vo = Counter()
-    vo = get_vocab(token_raw, vo)
-    vo = get_vocab(token_new, vo)
+    vo, token_raw_list = get_vocab(token_raw, vo)
+    vo, token_new_list = get_vocab(token_new, vo)
     vocab_index = generate_vocab(vo)
-    t_index_raw = index_token(token_raw, vocab_index)
-    t_index_new = index_token(token_new, vocab_index)
-    t_dict_raw = {0:t_index_raw}
-    t_dict_new = {0:t_index_new}
-    bleu, rouge_l = eval_accuracies(t_dict_raw, t_dict_new)
-    print(bleu, rouge_l)
+    vocab_len = len(vocab_index)
+    print('vocab size: ', vocab_len)
+    t_index_raw = index_token(token_raw_list, vocab_index)
+    t_index_new = index_token(token_new_list, vocab_index)
+    string_raw = generate_string(t_index_raw)
+    string_new = generate_string(t_index_new)
+    if vocab_len > 1000:
+        print('文本较长，采用编辑距离计算相似度')
+        l_similarity = levenshtein_similarity(string_raw, string_new)
+        print(f'相似度：{l_similarity}')
+    else:
+        print('文本较长，采用bleu和rouge_l方法计算相似度')
+        t_dict_raw = {0:[string_raw]}
+        t_dict_new = {0:[string_new]}
+        bleu, rouge_l = eval_accuracies(t_dict_raw, t_dict_new)
+        print(f'bleu相似度：{bleu}, rouge_l相似度：{rouge_l}')
 
 
 if __name__ == '__main__':
-    a = {0:['我 like English well . How do you ?']}
-    b = {0:['我 like English well . How do me ?']}
-    a = {0: ['一位真正的作家永远只为内心写作']}
-    b = {0: ['1位真正的作家永远只为内心写作。']}
-    bleu, rouge_l = eval_accuracies(a, b)
-    print(bleu, rouge_l)
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--raw_file', type=str, default=r'D:\我的文档\Desktop\学校\软件工程\作业\2\orig.txt')
+    parser.add_argument('--compare_file', type=str, default=r'D:\我的文档\Desktop\学校\软件工程\作业\2\orig_0.8_add.txt')
+    args = parser.parse_args()
+    main_process(args.raw_file, args.compare_file)
